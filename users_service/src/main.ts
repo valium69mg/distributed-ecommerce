ca2bbd57f0991738d887fae9979984e65fb1d47e
valium69mg <xdeus.v2@gmail.com>
2025-10-17 18:59:54 -0600

    mod: add project to bind to all interfaces

--- users_service/src/main.ts
+++ users_service/src/main.ts
@@ -5,6 +5,8 @@ import { AppDataSource } from "./data-source";
 async function bootstrap() {
   await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, "0.0.0.0", () => {
+    console.log("Server running on port 3000");
+  });
 }
 bootstrap();
