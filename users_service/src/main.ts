e72ce3b0df2a490d4848bcc4deffebf0ac77aacb
valium69mg <xdeus.v2@gmail.com>
2025-10-18 21:13:13 -0600

    feat: add CORS config

--- users_service/src/main.ts
+++ users_service/src/main.ts
@@ -5,6 +5,11 @@ import { AppDataSource } from "./data-source";
 async function bootstrap() {
   await AppDataSource.initialize();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: "*",
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000, "0.0.0.0", () => {
     console.log("Server running on port 3000");
   });
